59a6858e6d8eec83c44c8efd711028f719410ce8
Ramón Masek <ramon.masek@fokus.fraunhofer.de>
2011-02-17 12:05:33 +0100

    add settings.h/.c holding configuration member

---
+++ settings.c
@@ -0,0 +1,59 @@
+/**
+ * @file settings.c
+ * impd4e - configuration and getter/setter functions
+ * Copyright (c) 2010, Fraunhofer FOKUS (Carsten Schmoll, Ramon Massek) \
+ *                     & TU-Berlin (Christian Henke)
+ * Copyright (c) 2010, Robert Wuttke <flash@jpod.cc>
+ *
+ * Code within #ifdef verbose [..] #endif: 
+ *                                   (c) 2005-10 - Luca Deri <deri@ntop.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation either version 3 of the License, or (at your option) any
+ * later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+//#include <stdlib.h>
+//#include <stdint.h>
+//#include <unistd.h>
+//#include <errno.h>
+//
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <sys/ioctl.h>
+//#include <fcntl.h>
+//#include <linux/if.h>
+//#include <netinet/in.h>
+//
+//#ifndef PFRING
+//#include <pcap.h>
+//#endif
+//
+//#include <string.h>
+//
+//#ifdef PFRING
+//#include <netinet/ip.h>
+//#include <net/ethernet.h>     /* the L2 protocols */
+//#include <pf_plugin_impd4e.h>
+//#endif
+//
+//#include "mlog.h"
+//#include "logger.h"
+//#include "helper.h"
+//#include "constants.h"
+
+
+
+
+
+
---
+++ settings.h
@@ -0,0 +1,37 @@
+/*
+ * impd4e - a small network probe which allows to monitor and sample datagrams
+ * from the network and exports hash-based packet IDs over IPFIX
+ *
+ * Copyright (c) 2010, Fraunhofer FOKUS (Ramon Massek)
+ * Copyright (c) 2010, Robert Wuttke <flash@jpod.cc>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation either version 3 of the License, or (at your option) any
+ * later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+//#include <stdint.h>
+//#include <limits.h>
+//#include <errno.h>
+//#include <sys/types.h>
+
+//#include "constants.h"
+
+
+
+
+
+#endif /* SETTINGS_H_ */
+

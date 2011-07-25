101de15350a59b94d5c7d30750ce552899a1e153
Ramón Masek <ramon.masek@fokus.fraunhofer.de>
2011-07-25 13:18:30 +0200

    backup ptm folder before merge master branch, to keep track of ptm while being able to merge master branch without losing ptm folder later on

---
+++ ptm.bak/lib/HTMLFrontend/T1EntitySerializer.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from ptmhub.restfrontend import BaseSerializer
+from T1Entity import T1Entity
+
+class T1EntitySerializer(BaseSerializer):
+	def __init__(self, t1client):	
+		prefix = "hello"
+		super(T1EntitySerializer, self).__init__(prefix)
+		self.t1client = t1client
+
+	def _unserialize_reference(self, id):
+		t1Ent = T1Entity(id, self.t1client, None)
+		return t1Ent
+	
+	# assuming config dict is a dict of dicts
+	#def serialize(self, config_dict):
+	#	raise NotImplementedError()
+

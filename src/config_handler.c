a6354e2d7d94f596f65f11301bdcfeac513632f4
Ramón Masek <ramon.masek@fokus.fraunhofer.de>
2012-06-13 16:16:49 +0200

    add missing license header

--- src/config_handler.c
+++ src/config_handler.c
@@ -1,3 +1,37 @@
+/*
+ * impd4e - a small network probe which allows to monitor and sample datagrams
+ * from the network based on hash-based packet selection.
+ *
+ * Copyright (c) 2011
+ *
+ * Fraunhofer FOKUS
+ * www.fokus.fraunhofer.de
+ *
+ * in cooperation with
+ *
+ * Technical University Berlin
+ * www.av.tu-berlin.de
+ *
+ * authors:
+ * Ramon Masek <ramon.masek@fokus.fraunhofer.de>
+ * Christian Henke <c.henke@tu-berlin.de>
+ * Carsten Schmoll <carsten.schmoll@fokus.fraunhofer.de>
+ *
+ * For questions/comments contact packettracking@fokus.fraunhofer.de
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include <unistd.h> // STDIN_FILENO
 #include <string.h> //strlen
